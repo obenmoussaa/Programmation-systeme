c53442d6ca7e87f427504927e96a6de1ca2b7946
BEN MOUSSA Oussama <oussama.ben_moussa@bordeaux-inp.fr>
2023-12-15 08:37:24 -0500

    td6

---
+++ prog_sys/td6/shm.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include"utils.h"
+#define FILE_PATH "/etc/bashrc"
+#define SIZE 1024
+
+int main() {
+    key_t key;
+    int shmid;
+    void *shm_ptr;
+    // Création d'une clé IPC avec ftok
+    key = ftok("/etc/bash.bashrc" , 'R');
+    exit_if(key == -1, "ftok");
+    //shmget
+    shmid = shmget(key, SIZE, IPC_CREAT | 0644);
+    exit_if( shmid == -1, "shmid");
+    //shmadd
+    
+    shm_ptr = shmat(shmid, NULL, 0);
+    exit_if (shm_ptr == (void *)(-1), "shmmat");
+    *((int *)shm_ptr)+=1;
+    printf("Valeur incrémentée : %d\n", *((int *)shm_ptr));
+    exit_if(shmdt(shm_ptr) == -1, "shmd");
+
+    
+    exit(EXIT_SUCCESS);
+}
+
---
+++ prog_sys/td6/threads.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <pthread.h>
+#include "utils.h"
+
+#define NUMBER_THREAD 8 
+
+static int global = 0;
+void *start(void *pid) {
+    int local = 0;
+    local++;
+    global++;
+    printf("variable globale : %d %p\n", global, &global);
+    printf("varibale locale: %d %p\n", local, &local);
+    printf("le pid du processus %p\n", pid);
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t thread[NUMBER_THREAD];
+    for (int i = 0; i < NUMBER_THREAD; i++) {
+        pid_t pid = getpid();
+        pthread_create(&thread[i], NULL, start, &pid);
+    }
+    for (int i = 0; i < NUMBER_THREAD; i++) {
+        pthread_join(thread[i], NULL);
+    }
+    printf("C'est le Thread principal\n");
+    return EXIT_SUCCESS;
+}
+
